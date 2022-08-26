94d4cec1cec6630ee23e0cf615521528a517d762
Alfred-Onuada <aonuada5@gmail.com>
2022-08-26 11:43:22 +0100

    working on docker compose

---
+++ admin/app/index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const compression = require('compression');
+
+app.use(compression());
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', () => {
+  res.json({ message: 'Hurray!! admin service works' });
+})
+
+app.listen(PORT, () => console.log(`Admin service is live at http://localhost:${PORT}`))
